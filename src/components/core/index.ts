f99c8bf317029dfc5272b075de39de232bbab5c6
bnmwag <bnm.wag@gmail.com>
2026-09-10 20:40:29 +0200

    refactor(core): fix exports and section padding
    - remove exports for image and button components that do not exist
    - use the grid margin token for section padding

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,5 +1,3 @@
 export { default as Container } from "./container.astro";
 export { default as Section } from "./section.astro";
 export { default as Grid } from "./grid.astro";
-export { default as Image } from "./image.astro";
-export { default as Button } from "./button.astro";
